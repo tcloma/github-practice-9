c32df92f96aae063e2ae1fdd451ae8324920d8d7
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2022-06-29 09:32:05 -0700

    Added CSS Styling and JavaScript function to change text content

---
+++ main.js
@@ -0,0 +1,7 @@
+const centerText = document.querySelector("#center-text")
+const pageBody = document.querySelector("#page-body")
+
+centerText.addEventListener('click', () => {
+    centerText.style.color = 'white';
+    centerText.textContent = `I'm sorry-`;
+})
